ae52f382ec528181324b1748f25bf85bdb192f42
alif <aliffirdhauz@gmail.com>
2019-11-02 21:42:11 +0700

    adding table

---
+++ app/src/main/java/com/kucingselfie/kotlindicodingsubmission2/model/LastMatchFavorite.kt
@@ -0,0 +1,16 @@
+package com.kucingselfie.kotlindicodingsubmission2.model
+
+class LastMatchFavorite(
+    val id: Long?,
+    val matchId: String?,
+    val matchName: String?,
+    val matchPicture: String
+) {
+    companion object {
+        const val TABLE_LAST_MATCH_FAVORITE: String = "TABLE_LAST_MATCH_FAVORITE"
+        const val ID: String = "ID_"
+        const val MATCH_ID: String = "MATCH_ID"
+        const val MATCH_NAME: String = "MATCH_NAME"
+        const val MATCH_PICTURE: String = "MATCH_PICTURE"
+    }
+}
---
+++ app/src/main/java/com/kucingselfie/kotlindicodingsubmission2/model/NextMatchFavorite.kt
@@ -0,0 +1,16 @@
+package com.kucingselfie.kotlindicodingsubmission2.model
+
+data class NextMatchFavorite(
+    val id: Long?,
+    val matchId: String?,
+    val matchName: String?,
+    val matchPicture: String
+) {
+    companion object {
+        const val TABLE_NEXT_MATCH_FAVORITE: String = "TABLE_NEXT_MATCH_FAVORITE"
+        const val ID: String = "ID_"
+        const val MATCH_ID: String = "MATCH_ID"
+        const val MATCH_NAME: String = "MATCH_NAME"
+        const val MATCH_PICTURE: String = "MATCH_PICTURE"
+    }
+}
